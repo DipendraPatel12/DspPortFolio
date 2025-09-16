a2d020df62cfd0cd8162c4c9cc4cdac9c8d3e09e
dipendra patel <dipendrapatel926@gmail.com>
2025-09-16 22:16:51 +0530

    minor change

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,7 +35,7 @@ const Projects = () => {
         "EXPRESS",
         "MongoDB",
       ],
-      link: "https://poster-z.vercel.app",
+      link: "https://poster-z-ecommerce.vercel.app",
     },
   
   ];
